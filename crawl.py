b50460a3c00724c9ae6f6fe90b809d57315982aa
perplx <perplx@users.noreply.github.com>
2024-07-14 14:23:52 -0400

    Added documentation, cleanup.

--- crawl.py
+++ crawl.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 
-# see: https://youtu.be/ftmdDlwMwwQ
+"""Basic Web-Crawler using Python async.
+see: https://youtu.be/ftmdDlwMwwQ
+"""
 
+
+# standard imports
 import asyncio
 import html.parser
 import pathlib
@@ -9,10 +13,13 @@ import time
 import urllib.parse
 from typing import Callable, Iterable
 
+# library imports
 import httpx
 
 
 class UrlFilterer:
+    """Filter URLs for crawling."""
+
     def __init__(
             self,
             allowed_domains: set[str] | None = None,
@@ -36,6 +43,7 @@ class UrlFilterer:
         # filter URL based on domain
         if self.allowed_domains is not None and parsed.netloc not in self.allowed_domains:
             return None
+        
         # filter URL based on file-extension
         ext = pathlib.Path(parsed.path).suffix
         if self.allowed_filetypes is not None and ext not in self.allowed_filetypes:
@@ -45,6 +53,8 @@ class UrlFilterer:
 
 
 class UrlParser(html.parser.HTMLParser):
+    """Parse HTML page to extract the URLKs in `<a>` tags."""
+
     def __init__(
             self,
             base: str,
@@ -55,22 +65,14 @@ class UrlParser(html.parser.HTMLParser):
         self.filter_url = filter_url
         self.found_links = set()
 
-    # def handle_starttag(self, tag: str, attrs: list[tuple[str, str | None]]) -> None:
-    #     if tag != "a":
-    #         return
-        
-    #     for attr, url in attrs:
-    #         if attr != "href":
-    #             continue
-    #         if (url := self.filter_url(self.base, url)) is not None:
-    #             self.found_links.add(url)
-
     def handle_starttag(self, tag: str, attrs: list[tuple[str, str | None]]) -> None:
         if tag == "a":
             for attr, value in attrs:
                 if attr == "href":
                     if (url := self.filter_url(self.base, value)) is not None:
                         self.found_links.add(url)
+
+
 class Crawler:
     def __init__(
             self,
@@ -93,12 +95,12 @@ class Crawler:
         self.total = 0
 
     async def run(self):
-        """"""
-
+        # create all the workers and wait for them to be done
         await self.on_found_links(self.start_urls)  # prime the queue
         workers = [asyncio.create_task(self.worker()) for _ in range(self.num_workers)]
         await self.todo.join()
 
+        # once all the work is done, cancel all the workers
         for worker in workers:
             worker.cancel()
 
@@ -110,16 +112,16 @@ class Crawler:
                 return
 
     async def process_one(self):
-        """"""
+        """Process a single URL from the queue."""
 
-        # get a rask to do
+        # get a task to do
         url = await self.todo.get()
 
         try:
             # do the work
             await self.crawl(url)
         except Exception as e:
-            # retry handling:
+            # TODO retry handling:
             pass
         finally:
             # mark task as complete
@@ -129,23 +131,31 @@ class Crawler:
         # rate limiting
         await asyncio.sleep(1.1)
 
+        # retrieve the page at the given URL
         response = await self.client.get(url, follow_redirects=True)
 
+        # extract all the URLs from the links in the given page
         found_links = await self.parse_links(
             base=str(response.url),
             text=response.text,
         )
 
+        # add found URLs to the queue
         await self.on_found_links(found_links)
 
         self.done.add(url)
 
     async def parse_links(self, base: str, text: str) -> set[str]:
+        """Extract the URLs from the links in the given page text."""
+
         parser = UrlParser(base, self.filter_url)
         parser.feed(text)
         return parser.found_links
 
     async def on_found_links(self, urls: set[str]):
+        """When new URLs are found on a crawled page, add them to hte queue."""
+
+        # get the new URLs, which haven't been seen yet
         new = urls - self.seen
         self.seen.update(new)
 
@@ -163,12 +173,14 @@ class Crawler:
 
 
 async def main():
+    # select which URLs will be crawled
     filterer = UrlFilterer(
         allowed_domains={"mcoding.io"},
         allowed_schemes={"http", "https"},
         allowed_filetypes={".html", ".php", ""},
     )
 
+    # crawl from the given URLs
     start = time.perf_counter()
     async with httpx.AsyncClient() as client:
         crawler = Crawler(
@@ -181,6 +193,7 @@ async def main():
         await crawler.run()
     end = time.perf_counter()
 
+    # show statistics about the crawl
     seen = sorted(crawler.seen)
     print("results:")
     for url in seen:
